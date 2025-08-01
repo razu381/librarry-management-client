5bdeb0aff3cd512ddad2171ebf82ee3404b69623
razu381 <sirazu52@gmail.com>
2025-08-01 11:46:23 +0600

    Base url changed

--- src/Redux/api/baseApi.ts
+++ src/Redux/api/baseApi.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://library-management-server-five-beta.vercel.app/api/",
+  }),
   tagTypes: ["Books", "BorrowSummary"],
   endpoints: () => ({}),
 });
